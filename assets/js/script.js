e6c7573993486deff562d3b8db668f141a646984
RenggaRizky <renggarizkys@gmail.com>
2023-02-13 22:40:41 +0700

    perbaiki responsive pada navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-const hamburger = document.querySelector(".hamburger");
+const hamburger = document.querySelector("input[name=hamburger]");
+const navMenu = document.querySelector(".nav-menu");
+const btnNav = document.querySelector(".btn-nav");
+const icon = document.querySelector("i.fa");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("hidden");
+    btnNav.classList.toggle("hidden");
+
+    if (hamburger.checked) {
+        icon.classList.replace("fa-bars", "fa-close");
+    } else {
+        icon.classList.replace("fa-close", "fa-bars");
+    }
+});
