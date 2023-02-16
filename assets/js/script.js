606fb4e3207a196753d916f5d06d77ad21e64c27
RenggaRizky <renggarizkys@gmail.com>
2023-02-16 22:45:46 +0700

    tambah header dan content section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@ const hamburger = document.querySelector("input[name=hamburger]");
 const navMenu = document.querySelector(".nav-menu");
 const btnNav = document.querySelector(".btn-nav");
 const icon = document.querySelector("i.fa");
+const navbar = document.querySelector('.navbar')
 
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("hidden");
     btnNav.classList.toggle("hidden");
+    navbar.classList.toggle('navbar-active')
 
     if (hamburger.checked) {
         icon.classList.replace("fa-bars", "fa-close");
